f6fde00327f244825e13fbe0a9093b2ffdf20b18
BATTLEHAWK <154075059@qq.com>
2020-04-10 20:41:59 +0800

    添加网络框架(Mirror)

---
+++ Assets/Scripts/GameFrameWork/Network(Mirror)/Runtime/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Mirror.Tests")]
+[assembly: InternalsVisibleTo("Mirror.Tests.Runtime")]
+[assembly: InternalsVisibleTo("Mirror.Tests.Performance")]
+[assembly: InternalsVisibleTo("Mirror.Tests.Performance.Runtime")]
